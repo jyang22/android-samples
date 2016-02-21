c7f9f4f75a4cde8d322968118339ba19f77e5611
jyang22 <YJ@as-Mac-Pro.attlocal.net>
2016-02-21 09:49:51 -0800

    FEB21

--- L08-AutoCompleteTextView/app/src/main/java/net/junsun/l08_autocompletetextview/MainActivity.java
+++ L08-AutoCompleteTextView/app/src/main/java/net/junsun/l08_autocompletetextview/MainActivity.java
@@ -1,12 +1,15 @@
 package net.junsun.l08_autocompletetextview;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,28 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String[] countries = getResources().getStringArray(R.array.countries);
-
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(
-                        this,
-                        // android.R.layout.simple_dropdown_item_1line,
-                        android.R.layout.simple_list_item_single_choice,
-                        countries);
-
-        final AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
-        autoCompleteTextView.setAdapter(adapter);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toast("Country is " + autoCompleteTextView.getText());
-            }
-        });
-    }
-
-    void toast(String msg) {
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
+
+public class SimpleListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] items = {"alecgator", "goat", "rabbit"};
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Log.d("COEN268", "Item clicked: " + position);
+    }
+}
+
